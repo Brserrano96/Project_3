049636cf22d70ecb701b07c395b66f8fbb1a1810
Brserrano96 <brandonserrano1212@yahoo.com>
2017-05-03 20:57:22 -0700

    Create Guild.h

---
+++ Project_3/Guild.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include <map>
+#include "Mage.h"
+#include "Paladin.h"
+#include "Ranger.h"
+#include "Warrior.h"
+using namespace std;
+
+class Guild
+{
+	private:
+		vector<shared_ptr<Actor>> npcs;
+		string name;
+		int gold = 0;
+		int rangerCount = 0;
+		int warriorCount = 0;
+		int paladinCount = 0;
+		int mageCount = 0;
+
+	public:
+		Guild(string name);
+		~Guild();
+
+		string getName() const { return name; }
+		void AddNPC(shared_ptr<Actor> _npc);
+
+		string getInfo();
+		string MagesAttack();
+		string PaladinsAttack();
+		string RangersAttack();
+		string WarriorsAttack();
+		string AllAdventurersAttack();
+};
